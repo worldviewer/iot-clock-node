05049dd3b6cb7c689ad4ce2c4c6e8ca60854738b
Chris Reeve <paradigmsareconstructed@gmail.com>
2015-07-17 17:54:12 -0700

    Calculated alarm time in seconds since 1970

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 var express = require("express"),
     ejs = require("ejs"),
     request = require('request'),
+    bodyParser = require('body-parser'),
     app = express();
 
 app.set('view engine', 'ejs');
+
+// The "extended" syntax allows for rich objects and arrays to 
+// be encoded into the URL-encoded format, allowing for a 
+// JSON-like experience with URL-encoded.
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static(__dirname + '/public'));
 
@@ -13,7 +19,15 @@ app.get('/', function (req, res) {
 
 var url = 'https://agent.electricimp.com/h__trcroDG77?led=0';
 
+// Things we want to receive:
+// Days of the week
+// Hours
+// Minutes
+// on-off State
+// Snooze Time
 app.post('/led', function (req, res) {
+	console.log(req.body);
+
 	request(url, function(err, resp, body) {
 		if(!err && resp.statusCode === 200) {
 			console.log("good!");
--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,9 @@
 $(document).ready(function() {
+    var onoff = false;
+    var hours;
+    var minutes;
+    var ampm;
+
     // *** SNOOZE SLIDER ***
     $('#ex1').slider({
         formatter: function(value) {
@@ -9,6 +14,83 @@ $(document).ready(function() {
     // *** DAY SELECTOR ***
     $('.btn[type="button"]').click(function(){
         $(this).toggleClass('grey');
+    });
+
+    // $("form").submit(function() {
+    //     console.log("worked");
+    //     $('.icon').hasClass('on').submit();
+    //     return true;
+    // });
+
+    $('#sync-btn').on('click', function() {
+        var seconds = parseInt((new Date()).getTime()/1000);
+
+        // Get the current date
+        var objToday = new Date(),
+            weekday = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'),
+            dayOfWeek = weekday[objToday.getDay()],
+            domEnder = new Array( 'th', 'st', 'nd', 'rd', 'th', 'th', 'th', 'th', 'th', 'th' ),
+            dayOfMonth = today + (objToday.getDate() < 10) ? '0' + objToday.getDate() + domEnder[objToday.getDate()] : objToday.getDate() + domEnder[parseFloat(("" + objToday.getDate()).substr(("" + objToday.getDate()).length - 1))],
+            months = new Array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'),
+            curMonth = months[objToday.getMonth()],
+            curYear = objToday.getFullYear(),
+            curHour = objToday.getHours() > 12 ? objToday.getHours() - 12 : (objToday.getHours() < 10 ? "0" + objToday.getHours() : objToday.getHours()),
+            curMinute = objToday.getMinutes() < 10 ? "0" + objToday.getMinutes() : objToday.getMinutes(),
+            curSeconds = objToday.getSeconds() < 10 ? "0" + objToday.getSeconds() : objToday.getSeconds(),
+            curMeridiem = objToday.getHours() > 12 ? "PM" : "AM";
+        var today = curHour + ":" + curMinute + "." + curSeconds + curMeridiem + " " + dayOfWeek + " " + dayOfMonth + " of " + curMonth + ", " + curYear;
+
+        // Grab days of the week
+        var monday = $('#monday').hasClass('grey');
+        var tuesday = $('#tuesday').hasClass('grey');
+        var wednesday = $('#wednesday').hasClass('grey');
+        var thursday = $('#thursday').hasClass('grey');
+        var friday = $('#friday').hasClass('grey');
+        var saturday = $('#saturday').hasClass('grey');
+        var sunday = $('#sunday').hasClass('grey');
+
+        // Snooze Time
+        var snooze = $('#ex1').val()*60;
+
+        // The problem is that we can get current time in "Epoch UNIX seconds to 1970",
+        // but we are setting our alarm relative to the last midnight.  So, we need to
+        // send the imp the number of seconds to the alarm, since 1970.
+
+        console.log("today: "+seconds);
+        console.log("today, hours, in seconds: "+curHour*60*60);
+        console.log("today, minutes, in seconds: "+curMinute*60);
+
+        // To get seconds from 1970 to last midnight, subtract hours, minutes and seconds
+        // (in seconds)
+        var midnight = seconds - curHour*3600 - curMinute*60 - curSeconds;
+        midnight = (curMeridiem == "AM") ? midnight : (midnight - 43200);
+        console.log("today's midnight, in seconds: "+ midnight);
+
+        // alarmSeconds is number of seconds since midnight to alarm
+        var alarmSeconds = hours*3600 + minutes*60;
+        alarmSeconds = (ampm === "am") ? alarmSeconds : alarmSeconds + 43200;
+        console.log("alarm seconds to midnight: "+alarmSeconds);
+
+        // The value we want to send to the imp is the sum of our alarm seconds
+        // with seconds since 1970 to last midnight
+        var impSeconds = midnight + alarmSeconds;
+        console.log("impSeconds: "+impSeconds);
+
+        var dataString = 'onoff='+onoff+'&monday='+monday+'&tuesday='+
+            tuesday+'&wednesday='+wednesday+'&thursday='+thursday+'&friday='+
+            friday+'&saturday='+saturday+'&sunday='+sunday+'&seconds='+
+            impSeconds+'&snooze='+snooze;
+
+        console.log(dataString);
+
+        // $.ajax({
+        //     type: "POST",
+        //     url: 'led',
+        //     data: dataString,
+        //     success: function() {
+
+        //     }
+        // })
     });
 
     // *** SYNC BUTTON ***
@@ -22,13 +104,136 @@ $(document).ready(function() {
         $(function () {
             return $('.icon').on('click', function () {
                 if ($(this).hasClass('on')) {
+                    onoff = false;
                     return $(this).removeClass('on');
                 } else {
+                    onoff = true;
                     return $(this).addClass('on');
                 }
             });
         });
     }.call(this));
+
+    $(".knob.hour").knob({
+        change : function (value) {
+
+            hours = Math.round(value % 12);
+            hours = (hours === 0) ? 12 : hours;
+
+            $("#hour-label").text(hours);
+
+            // var hr = Math.floor(hours);
+
+            ampm = value >= 12.0 ? "pm": "am";
+
+            $("#am-pm").text(ampm);
+
+            console.log(Math.floor(value/2))
+            console.log(value);
+
+
+        },
+        release : function (value) {
+            hours = parseInt(value % 12);
+
+        },
+        cancel : function () {
+            console.log("cancel : ", this);
+        },
+        /*format : function (value) {
+            return value + '%';
+        },*/
+        draw : function () {
+
+            // "tron" case
+            if(this.$.data('skin') == 'tron') {
+
+                this.cursorExt = 0.3;
+
+                var a = this.arc(this.cv)  // Arc
+                    , pa                   // Previous arc
+                    , r = 1;
+
+                this.g.lineWidth = this.lineWidth;
+
+                if (this.o.displayPrevious) {
+                    pa = this.arc(this.v);
+                    this.g.beginPath();
+                    this.g.strokeStyle = this.pColor;
+                    this.g.arc(this.xy, this.xy, this.radius - this.lineWidth, pa.s, pa.e, pa.d);
+                    this.g.stroke();
+                }
+
+                this.g.beginPath();
+                this.g.strokeStyle = r ? this.o.fgColor : this.fgColor ;
+                this.g.arc(this.xy, this.xy, this.radius - this.lineWidth, a.s, a.e, a.d);
+                this.g.stroke();
+
+                this.g.lineWidth = 2;
+                this.g.beginPath();
+                this.g.strokeStyle = this.o.fgColor;
+                this.g.arc( this.xy, this.xy, this.radius - this.lineWidth + 1 + this.lineWidth * 2 / 3, 0, 2 * Math.PI, false);
+                this.g.stroke();
+
+                return false;
+            }
+        }
+    });
+
+    $(".knob.minute").knob({
+        change : function (value) {
+            minutes = Math.round(value);
+            minutes = (minutes < 10) ? "0" + minutes : minutes;
+            $("#min-label").text(minutes);
+
+            console.log("mins: " + minutes);
+        },
+        release : function (value) {
+            //console.log(this.$.attr('value'));
+            console.log("release : " + value);
+        },
+        cancel : function () {
+            console.log("cancel : ", this);
+        },
+        /*format : function (value) {
+            return value + '%';
+        },*/
+        draw : function () {
+
+            // "tron" case
+            if(this.$.data('skin') == 'tron') {
+
+                this.cursorExt = 0.3;
+
+                var a = this.arc(this.cv)  // Arc
+                    , pa                   // Previous arc
+                    , r = 1;
+
+                this.g.lineWidth = this.lineWidth;
+
+                if (this.o.displayPrevious) {
+                    pa = this.arc(this.v);
+                    this.g.beginPath();
+                    this.g.strokeStyle = this.pColor;
+                    this.g.arc(this.xy, this.xy, this.radius - this.lineWidth, pa.s, pa.e, pa.d);
+                    this.g.stroke();
+                }
+
+                this.g.beginPath();
+                this.g.strokeStyle = r ? this.o.fgColor : this.fgColor ;
+                this.g.arc(this.xy, this.xy, this.radius - this.lineWidth, a.s, a.e, a.d);
+                this.g.stroke();
+
+                this.g.lineWidth = 2;
+                this.g.beginPath();
+                this.g.strokeStyle = this.o.fgColor;
+                this.g.arc( this.xy, this.xy, this.radius - this.lineWidth + 1 + this.lineWidth * 2 / 3, 0, 2 * Math.PI, false);
+                this.g.stroke();
+
+                return false;
+            }
+        }
+    });
 
 });
       
